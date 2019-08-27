cfefe1adfe31052e50aa0ae6bca4c23cf360647c
erimsengezer <erimsengezer@gmail.com>
2019-08-27 13:02:38 +0300

    Initial Commit

---
+++ SCalculate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SCalculate
+//
+//  Created by Erim Şengezer on 27.08.2019.
+//  Copyright © 2019 Erim Şengezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
